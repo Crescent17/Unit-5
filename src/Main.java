c1dc028f1dc28b72b42c0baf4c288f189c342651
Crescent17 <maratos2001@mail.ru>
2021-12-22 10:19:52 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        char switchValue = 'E';
+        switch (switchValue) {
+            case 'A':
+                System.out.println("Character 'A' was found");
+                break;
+
+            case 'B':
+                System.out.println("Character 'B' was found");
+                break;
+
+            case 'C':
+                System.out.println("Character 'C' was found");
+                break;
+
+            case 'D':
+                System.out.println("Character 'D' was found");
+                break;
+
+            case 'E':
+                System.out.println("Character 'E' was found");
+                break;
+
+            default:
+                System.out.println("None of these characters were found");
+                break;
+        }
+    }
+}
