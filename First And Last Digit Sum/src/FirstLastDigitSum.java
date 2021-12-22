e4fd007024f7fa5332a0f7b522caa2b09c4d79e0
Marat <94042173+Crescent17@users.noreply.github.com>
2021-12-22 12:59:58 +0300

    Add files via upload

---
+++ First And Last Digit Sum/src/FirstLastDigitSum.java
@@ -0,0 +1,16 @@
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int sum = 0;
+        sum += number % 10;
+        while (number != 0) {
+            if (number / 10 == 0) {
+                sum += number % 10;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
+}
